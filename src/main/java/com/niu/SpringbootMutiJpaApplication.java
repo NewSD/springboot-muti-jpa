3290e3fa1c833a0d6e5ab16cd35050f2e4fd6e76
18300603955 <oceaniu@163.com>
2019-06-15 21:33:27 +0800

    init

---
+++ src/main/java/com/niu/SpringbootMutiJpaApplication.java
@@ -0,0 +1,13 @@
+package com.niu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMutiJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMutiJpaApplication.class, args);
+	}
+
+}
